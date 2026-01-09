1f303ed9d263092f6429281071dcc28a8fc4deb7
Oskar <12718920+oskar2517@users.noreply.github.com>
2026-01-09 19:22:43 +0100

    implement types for llvm ir

---
+++ LeanSpl/IR.lean
@@ -0,0 +1,150 @@
+namespace IR
+
+inductive LLVMType where
+  | i64
+  | i1
+  | arr (size : Nat) (base : LLVMType)
+  | ref (base : LLVMType)
+  deriving Repr, Inhabited
+
+def LLVMType.toString : LLVMType -> String
+  | .i64 => "i64"
+  | .i1 => "i1"
+  | .arr size base => s!"[{size} x {LLVMType.toString base}]"
+  | .ref base => s!"{LLVMType.toString base}*"
+
+instance : ToString LLVMType where
+  toString := LLVMType.toString
+
+structure Label where
+  name: String
+  deriving Repr
+
+instance : ToString Label where
+  toString l := s!"L{l.name}"
+
+def Label.definition (l : Label) : String :=
+  s!"{l}:"
+
+structure Register where
+  name: String
+  deriving Repr
+
+instance : ToString Register where
+  toString r := s!"%{r.name}"
+
+def Register.addr (r : Register) : String :=
+  s!"{r}.addr"
+
+structure Global where
+  name: String
+  deriving Repr, Inhabited
+
+instance : ToString Global where
+  toString g := s!"@_{g.name}"
+
+inductive RelOp where
+  | eq
+  | ne
+  | slt
+  | sle
+  | sgt
+  | sge
+  deriving Repr
+
+instance : ToString RelOp where
+  toString
+  | .eq => "eq"
+  | .ne => "ne"
+  | .slt => "slt"
+  | .sle => "sle"
+  | .sgt => "sgt"
+  | .sge => "sge"
+
+structure Operand where
+  type: LLVMType
+  register: Register
+  deriving Repr
+
+instance : ToString Operand where
+  toString o := s!"{o.type} {o.register}"
+
+inductive Instruction where
+  | icmp (target : Register) (op : RelOp) (left right : Register)
+  | getelementptr (target : Register) (address : Register)
+  | store (value : Register) (address : Register)
+  | load (target: Register) (address : Register)
+  | br_con (condition : Register) (label_then : Label) (label_else : Label)
+  | br (label: Label)
+  | call (name : Global) (arguments : List Operand)
+  | add (target : Register) (left right : Register)
+  | sub (target : Register) (left right : Register)
+  | mul (target : Register) (left right : Register)
+  | sdiv (target : Register) (left right : Register)
+  | alloca (target : Register) (type : LLVMType)
+  | ret
+  deriving Repr
+
+instance : ToString Instruction where
+  toString
+  | .icmp target op left right => s!"{target} = icmp {op} i64 {left}, {right}"
+  | .getelementptr target address => s!"{target} = getelementptr i64, i64* {address}, i64 0"
+  | .store value address => s!"store i64 {value}, i64* {address}, i64 0"
+  | .load target address => s!"{target} = load i64, i64* {address}"
+  | .br_con condition label_then label_else => s!"br i1 {condition}, label {label_then}, label {label_else}"
+  | .br label => s!"br label {label}"
+  | .call name arguments => s!"call void {name}({String.intercalate ", " (arguments.map ToString.toString)})"
+  | .add target left right => s!"{target} = add i64 {left}, {right}"
+  | .sub target left right => s!"{target} = sub i64 {left}, {right}"
+  | .mul target left right => s!"{target} = mul i64 {left}, {right}"
+  | .sdiv target left right => s!"{target} = sdiv i64 {left}, {right}"
+  | .alloca target type => s!"{target} = alloca {type}, align 8"
+  | .ret => "ret void"
+
+inductive BodyElement where
+  | label (l : Label)
+  | instruction (i : Instruction)
+  deriving Repr
+
+instance : ToString BodyElement where
+  toString
+  | .label l => l.definition
+  | .instruction i => s!"    {i}"
+
+structure Function where
+  name: Global
+  parameters: List LLVMType
+  body: List BodyElement
+  deriving Repr, Inhabited
+
+instance : ToString Function where
+  toString f :=
+    let parameters := String.intercalate ", " (f.parameters.map ToString.toString)
+    let body := String.intercalate "\n" (f.body.map ToString.toString)
+    s!"define void {f.name}({parameters})" ++ "{\n" ++ body ++ "}\n"
+
+structure Declaration where
+  name: Global
+  parameters: List LLVMType
+  deriving Repr
+
+instance : ToString Declaration where
+  toString f :=
+    let parameters := String.intercalate ", " (f.parameters.map ToString.toString)
+    s!"declare void {parameters}"
+
+inductive ProgramElement where
+  | decl (d : Declaration)
+  | func (f : Function)
+  deriving Repr
+
+structure Program where
+  declarations: List Declaration
+  functions: List Function
+  deriving Repr
+
+instance : ToString Program where
+  toString p := String.intercalate "\n" (p.declarations.map ToString.toString)
+  ++ "\n\n" ++ String.intercalate "\n" (p.functions.map ToString.toString)
+
+end IR
