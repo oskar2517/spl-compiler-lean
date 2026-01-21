14b169367ea4161e272a3a129821ee626805393a
Oskar <12718920+oskar2517@users.noreply.github.com>
2026-01-21 17:07:38 +0100

    refactor semantic analysis

--- LeanSpl/SemanticAnalysis.lean
+++ LeanSpl/SemanticAnalysis.lean
@@ -2,112 +2,149 @@ import LeanSpl.Absyn
 import LeanSpl.Table
 
 namespace SemanticAnalysis
+
+def ensure (b : Bool) (msg : String) : Except String Unit :=
+  if b then .ok () else .error msg
+
 mutual
+
   def varType (var: Absyn.Variable) (table: Table.SymbolTable) : Except String Table.SplType :=
     match var with
-      | .named nv => do
-        let e ← table.lookup nv
+    | .named nv => do
+      let e ← table.lookup nv
 
-        match e with
-        | .var ve => pure ve.typ
-        | _ => .error s!"{nv} is not a variable"
-      | .index arr i => do
-        let arr ← varType arr table
-        let arrType ← match arr with
-          | .arr a => pure a
-          | .primitive _ => .error "attempting to index non array"
+      match e with
+      | .var ve => .ok ve.typ
+      | _ => .error s!"{nv} is not a variable"
 
-        let indexType ← checkExpr i table
+    | .index arr i => do
+      let typ ← varType arr table
+      let arrTyp ← match typ with
+        | .arr a => .ok a
+        | .primitive _ => .error "attempting to index non array"
 
-        let _ ← match indexType with
-          | .primitive pt => match pt with
-            | .bool => .error "index must be int, received bool"
-            | .int => pure ()
-          | .arr _  => .error "index must be int, received array"
+      let indexTyp ← checkExpr i table
 
-        pure <| arrType.base
+      let _ ← match indexTyp with
+        | .primitive pt => match pt with
+          | .bool => .error "index must be int, received bool"
+          | .int => .ok ()
+        | .arr _  => .error "index must be int, received array"
+
+      .ok arrTyp.base
 
   def checkExpr (expr: Absyn.Expr) (table: Table.SymbolTable) : Except String Table.SplType :=
     match expr with
-      | .bin op left right => do
-          let leftType ← checkExpr left table
-          let rightType ← checkExpr right table
+    | .bin op left right => do
+      let leftTyp ← checkExpr left table
+      let rightTyp ← checkExpr right table
 
-          let _ ← if leftType != rightType then .error s!"operand type mismatch"
+      ensure (leftTyp == rightTyp) "operand type mismatch"
 
-          pure <| Absyn.BinOp.typ op
-      | .un _ operand => do
-          let operandType ← checkExpr operand table
-          if operandType != (.primitive .int) then .error "operand for unary Minus must be int"
+      .ok <| Absyn.BinOp.typ op
 
-          pure <| .primitive .int
-      | .int _ => pure <| .primitive .int
-      | .var v => varType v table
+    | .un _ operand => do
+      let operandTyp ← checkExpr operand table
+      ensure (operandTyp == .primitive .int) "operand for unary Minus must be int"
+
+      .ok <| .primitive .int
+
+    | .int _ => .ok <| .primitive .int
+    | .var v => varType v table
+
 end
 
-def checkAssignStmt (target: Absyn.Variable) (value: Absyn.Expr) (table: Table.SymbolTable) : Except String Unit := do
-  let targetType ← varType target table
-  let valueType ← checkExpr value table
+def checkAssignStmt
+  (target: Absyn.Variable)
+  (value: Absyn.Expr)
+  (table: Table.SymbolTable)
+  : Except String Unit := do
 
-  let _ ← if targetType matches .arr _ || targetType != valueType then .error "target type does not match value type or you are trying to assign to an array"
+  let targetTyp ← varType target table
+  let valueTyp ← checkExpr value table
 
+  ensure (!(targetTyp matches .arr _)) "cannot assign to array"
+  ensure (targetTyp == valueTyp) "type mismatch"
 
-def checkCallStmt (name : String) (args: List Absyn.Expr) (table: Table.SymbolTable) (global: Table.SymbolTable): Except String Unit := do
+def checkCallStmt
+  (name : String)
+  (args: List Absyn.Expr)
+  (table: Table.SymbolTable)
+  (global: Table.SymbolTable)
+  : Except String Unit := do
+
   let e ← global.lookup name
 
   let proc ← match e with
-  | .proc pe => pure pe
+  | .proc pe => .ok pe
   | _ => .error s!"{name} is not a procedure"
 
-  let _ ← if args.length != proc.parameters.length then .error s!"{name}: number of arguments provided does not match the number of required parameters"
+  ensure (args.length == proc.parameters.length) s!"{name}: number of arguments provided does not match the number of required parameters"
 
-  let _ ← for (p, arg) in proc.parameters.zip args.reverse do
-    let argType ← checkExpr arg table
-    let _ ← match argType == p.typ with
-    | true => pure ()
-    | false => .error s!"{name}: argument type mismatch"
+  for (p, arg) in proc.parameters.zip args.reverse do
+    let argTyp ← checkExpr arg table
 
-    let _ ← if p.is_ref && !(arg matches .var _) then
-      .error "Argument to a reference parameter must be variable"
+    ensure (argTyp == p.typ) s!"{name}: argument type mismatch"
+
+    if p.is_ref then
+      ensure (arg matches .var _) "Argument to a reference parameter must be variable"
 
 mutual
-  partial def checkWhileStmt (cond: Absyn.Expr) (body: Absyn.Stmt) (table: Table.SymbolTable) (global: Table.SymbolTable): Except String Unit := do
+  partial def checkWhileStmt
+    (cond: Absyn.Expr)
+    (body: Absyn.Stmt)
+    (table: Table.SymbolTable)
+    (global: Table.SymbolTable)
+    : Except String Unit := do
+
     let type ← checkExpr cond table
     let _ ← match type with
       | .primitive p => match p with
-        | .bool => pure ()
+        | .bool => .ok ()
         | _ => .error "if condition must be Boolean, received int"
       | _  => .error "if condition must be boolean, received array"
 
-    let _ ← checkStatement body table global
+    let _ ← checkStmt body table global
 
 
-  partial def checkIfStmt (cond: Absyn.Expr) (thenBranch: Absyn.Stmt) (elseBranch: Option Absyn.Stmt) (table: Table.SymbolTable) (global: Table.SymbolTable): Except String Unit := do
-    let type ← checkExpr cond table
+  partial def checkIfStmt
+    (cond: Absyn.Expr)
+    (thenBranch: Absyn.Stmt)
+    (elseBranch: Option Absyn.Stmt)
+    (table: Table.SymbolTable)
+    (global: Table.SymbolTable)
+    : Except String Unit := do
 
-    let _ ← match type with
+    let typ ← checkExpr cond table
+
+    match typ with
       | .primitive p => match p with
-        | .bool => pure ()
+        | .bool => .ok ()
         | _ => .error "if condition must be Boolean, received int"
       | _  => .error "if condition must be boolean, received array"
 
-    let _ ← checkStatement thenBranch table global
-    let _ ← match elseBranch with
-      | some s => checkStatement s table global
-      | none => pure ()
+    checkStmt thenBranch table global
+    match elseBranch with
+      | some s => checkStmt s table global
+      | none => .ok ()
 
-  partial def checkStatement (s: Absyn.Stmt) (table: Table.SymbolTable) (global: Table.SymbolTable): Except String Unit :=
+  partial def checkStmt (s: Absyn.Stmt) (table: Table.SymbolTable) (global: Table.SymbolTable): Except String Unit :=
     match s with
       | .assign t v => checkAssignStmt t v table
       | .block b => checkBlock b table global
       | .call n args => checkCallStmt n args table global
-      | .empty => pure ()
+      | .empty => .ok ()
       | .if_ cond t e => checkIfStmt cond t e table global
       | .while_ cond b => checkWhileStmt cond b table global
 
-  partial def checkBlock (stmts: List Absyn.Stmt) (table: Table.SymbolTable) (global: Table.SymbolTable): Except String Unit := do
+  partial def checkBlock
+    (stmts: List Absyn.Stmt)
+    (table: Table.SymbolTable)
+    (global: Table.SymbolTable)
+    : Except String Unit := do
+
     for s in stmts do
-      let _ ← checkStatement s table global
+      checkStmt s table global
 
 end
 
@@ -115,19 +152,19 @@ def checkProcDef (d: Absyn.ProcDef) (table: Table.SymbolTable) : Except String U
   let e ← table.lookup d.name
 
   let proc ← match e with
-  | .proc pe => pure pe
+  | .proc pe => .ok pe
   | _ => unreachable!
 
   for s in d.body do
-    let _ ← checkStatement s proc.local_table table
+    checkStmt s proc.local_table table
 
-def checkGlobalDefinition (d: Absyn.GlobalDef) (table: Table.SymbolTable) : Except String Unit :=
+def checkGlobalDef (d: Absyn.GlobalDef) (table: Table.SymbolTable) : Except String Unit :=
   match d with
     | .procedure pd => checkProcDef pd table
-    | .type _ => pure ()
+    | .type _ => .ok ()
 
 def checkProgram (p: Absyn.Program) (table: Table.SymbolTable) : Except String Unit := do
-  for definition in p.definitions do
-    let _ ← checkGlobalDefinition definition table
+  for d in p.definitions do
+    let _ ← checkGlobalDef d table
 
 end SemanticAnalysis
